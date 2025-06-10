23117b9665d9936c4397effd2c8f076dd6692369
Marta Mączyńska <273761@student.pwr.edu.pl>
2025-06-10 15:06:39 +0200

    app.py

--- app.py
+++ app.py
@@ -1,22 +1,25 @@
 from flask import Flask, send_file, jsonify
 import pyodbc
 import os
+from dotenv import load_dotenv
+
+load_dotenv('hasla.env')
 
 app = Flask(__name__)
 
-# Konfiguracja połączenia SQL
-server = 'serwer-jezyki.database.windows.net,1433'
-database = 'bazaJezyki'
-username = 'adminlogin273761'
-password = 'Password273761'
-driver = '{ODBC Driver 18 for SQL Server}'
+# Konfiguracja połączenia SQL z pliku .env
+server = os.getenv('SQL_SERVER')
+database = os.getenv('SQL_DATABASE')
+username = os.getenv('SQL_USERNAME')
+password = os.getenv('SQL_PASSWORD')
+driver = 'ODBC Driver 18 for SQL Server'
 
 conn_str = f'DRIVER={driver};SERVER={server};DATABASE={database};UID={username};PWD={password};Encrypt=yes;TrustServerCertificate=no;Connection Timeout=30'
 conn = pyodbc.connect(conn_str)
 
 @app.route("/")
 def index():
-    return send_file("index.html")  # Wczytuje index.html z bieżącego folderu
+    return send_file("index.html")
 
 @app.route("/ile-niemiecki")
 def ile_niemiecki():
